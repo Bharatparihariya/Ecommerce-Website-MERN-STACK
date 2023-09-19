9fdd55d723f576a9824dfaebe15dc8da6944872b
Bharat Parihariya <bharatparihariya704@gmail.com>
2023-09-19 10:36:56 +0530

    Final Code
    Ecommerce App

--- config/db.js
+++ config/db.js
@@ -1,16 +1,14 @@
 import mongoose from "mongoose";
 import colors from "colors";
 const connectDb = async () => {
-    try {
-          const conn = await mongoose.connect(process.env.MONGO_URL);
-          console.log(`Conneted To Mongodb Database ${conn.connection.host}`.bgMagenta.white);
-    } catch (error) {
-        console.log(`Error in MongoDb ${error}`.bgRed.white);
-    }
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(
+      `Conneted To Mongodb Database ${conn.connection.host}`.bgMagenta.white
+    );
+  } catch (error) {
+    console.log(`Error in MongoDb ${error}`.bgRed.white);
+  }
 };
-   
-export default connectDb;
- 
 
- 
-     
+export default connectDb;
